04c126739abc40315eb066576fa4650dcec42205
Tudor <tudor.suditu@yahoo.com>
2018-05-20 18:30:10 +0300

    added finished event handler

--- utils/actionTypes.js
+++ utils/actionTypes.js
@@ -1,6 +1,6 @@
 
 function onLocationUpdate(ws, wss, payload) {
-    wss.clients.forEach(function each(client) {
+    wss.clients.forEach(function (client) {
         //simple broadcast
         if (client !== ws && client.readyState === client.OPEN) {
             //check if clients share the same eventId
@@ -15,12 +15,30 @@ function onLocationUpdate(ws, wss, payload) {
     });
 }
 
+function onFinishedEvent(ws, wss, payload) {
+    wss.clients.forEach(function (client) {
+        //simple broadcast
+        if (client !== ws && client.readyState === client.OPEN) {
+            //check if clients share the same eventId
+            if (client.eventId == ws.eventId) {
+                const messagePayload = {
+                    type: "finishedEvent",
+                    value: payload
+                };
+                client.send(JSON.stringify(messagePayload));
+            }
+        }
+    });
+}
+
 const actionTypes = {
     names: {
-        LOCATION_UPDATE: 'locationUpdate'
+        LOCATION_UPDATE: 'locationUpdate',
+        FINISHED_EVENT: 'finishedEvent'
     },
     handlers: {
         onLocationUpdate,
+        onFinishedEvent
     }
 }
 
